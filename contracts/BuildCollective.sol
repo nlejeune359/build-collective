b62b67044f589c251f7641ec0821a5113758ae75
Nicolas Lejeune <nicolaslejeune99@gmail.com>
2021-11-22 18:27:25 +0100

    projects handling

--- contracts/BuildCollective.sol
+++ contracts/BuildCollective.sol
@@ -54,10 +54,13 @@ contract BuildCollective is Ownable {
 
   /** Connection */
   event UserSignedUp(address indexed userAddress, User indexed user);
-  event CompanySignedUp(address indexed userAddress, Company indexed company);
   event UserSignedOut(address indexed userAddress);
-  event CompanySignedOut(address indexed userAddress);
+  event CompanySignedUp(address indexed companyAddress, Company indexed company);
+  event CompanySignedOut(address indexed companyAddress);
 
+  event ProjectCreated(address indexed userAddress, Project indexed project);
+  event ProjectRemoved(address indexed userAddress, Project indexed project);
+  event ContributorAddedToProject(address indexed userAddress, Project indexed project);
   /** Projects */
   // event NewProjectCreated(address indexed userAddress, Project indexed project);
 
@@ -84,18 +87,15 @@ contract BuildCollective is Ownable {
     
     companies[msg.sender] = Company(_username, 0, true);
     emit CompanySignedUp(msg.sender, companies[msg.sender]);
+    return companies[msg.sender];
   }
 
   function signUp(string memory _username) public returns (User memory) {
     require(bytes(_username).length > 0);
 
-    User memory u;
-    u.username = _username;
-    u.balance = 0;
-    u.registered = true;
-
-    users[msg.sender] = u;
+    users[msg.sender] = User(_username, 0, true);
     emit UserSignedUp(msg.sender, users[msg.sender]);
+    return users[msg.sender];
   }
 
   function signOut() public returns (bool) {
@@ -106,13 +106,13 @@ contract BuildCollective is Ownable {
     return true;
   }
 
-  // function signOutCompany() public returns (bool) {
-  //   require(companies[msg.sender].registered);
+  function signOutCompany() public returns (bool) {
+    require(companies[msg.sender].registered);
     
-  //   companies[msg.sender].registered = false;
-  //   emit CompanySignedOut(msg.sender);
-  //   return true;
-  // }
+    companies[msg.sender].registered = false;
+    emit CompanySignedOut(msg.sender);
+    return true;
+  }
 
   /** Transactions */
 
@@ -125,31 +125,51 @@ contract BuildCollective is Ownable {
 
   /** Projects */
 
-  // function getProjects() public returns (Project[] memory) {
-  //   require(users[msg.sender].isRegistered());
+  function getProjectIndex(address _userAddress, uint _id) private view returns (uint) {
+    Project[] memory p = projects[_userAddress];
+    for(uint i=0; i < p.length; i++) {
+      if (p[i].id == _id) {
+        return i;
+      }
+    }
+    return 0;
+  }
 
-  //   return users[msg.sender].getProjects();
-  // }
+  function getProjects() public view returns (Project[] memory) {
+    require(users[msg.sender].registered);
 
-  // function createProject(string memory _name) public returns (bool) {
-  //   require(users[msg.sender].isRegistered());
+    return projects[msg.sender];
+  }
 
-  //   users[msg.sender].addProject(new Project(_name));
-  //   return true;
-  // }
+  function createProject(string memory _name) public returns (Project memory) {
+    require(users[msg.sender].registered);
 
-  // function removeProject(uint256 _index) public returns (bool) {
-  //   require(users[msg.sender].isRegistered());
+    projects[msg.sender].push(Project(count, msg.sender, _name, 0));
+    count++;
+    uint index = projects[msg.sender].length - 1;
+    emit ProjectCreated(msg.sender, projects[msg.sender][index]);
+    return projects[msg.sender][index];
+  }
 
-  //   users[msg.sender].removeProject(_index);
-  //   return true;
-  // }
+  function removeProject(uint256 _id) public returns (bool) {
+    require(users[msg.sender].registered);
 
-  // function addContributorToProject(uint256 _index, address _contributor) public returns (bool) {
-  //   require(users[msg.sender].getProject(_index).getOwner() == msg.sender);
-  //   // require(users[_contributor]);
+    uint index = getProjectIndex(msg.sender, _id);
+    Project memory project = projects[msg.sender][index];
+    delete projects[msg.sender][index];
+    emit ProjectRemoved(msg.sender, project);
+    return true;
+  }
 
-  //   users[_contributor].addProject(users[msg.sender].getProject(_index));
-  //   return true;
-  // }
+  function addContributorToProject(uint256 _id, address _contributor) public returns (bool) {
+    require(_id >= 0 && _id < projects[msg.sender].length);
+    // require(users[_contributor].amount != 0);
+    require(projects[msg.sender][_id].owner == msg.sender);
+    // require(users[_contributor]);
+
+    uint index = getProjectIndex(msg.sender, _id);
+    Project memory p = projects[msg.sender][index];
+    projects[_contributor].push(p);
+    return true;
+  }
 }
